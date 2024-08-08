ce89eb801e6fb6c096d1bb623d3b8c4053e84ad7
Taiwo Kareem <13158672+tushortz@users.noreply.github.com>
2024-08-08 07:38:44 +0100

    Update mediafire.py

--- mediafire.py
+++ mediafire.py
@@ -1,61 +1,55 @@
-import json
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import argparse
 import os
-import requests
-import sys
 import re
-import click
+import sys
+
+import requests
+from tqdm import tqdm
+
+CHUNK_SIZE = 512 * 1024  # 512 KB
 
 
 def download_file(file_dict, folder_name="tmp"):
-    """Download file using the file data"""
-    
+    """Downloads the file using the file metadata"""
+
     filename = file_dict.get("filename")
     url = file_dict["links"]["normal_download"]
-    quick_key = file_dict["quickkey"]
-
-    print("Generating link ...")
 
     page = requests.get(url).text
-    regex = f'href="(http.*?/{quick_key}/.*?)"'
-    link = re.search(regex, page).group(1)
+    link = re.search(r'href="((http|https)://download[^"]+)', page)
+
+    if link:
+        link = link.group(1)
+    else:
+        print("Can't parse download link for {filename}")
+        return
+
     size = int(file_dict.get("size", "0"))
 
-    r = requests.get(link)
-    chunks = r.iter_content(chunk_size=1024)
+    res = requests.get(link, stream=True)
     filepath = f"{folder_name}/{filename}"
 
-    with click.progressbar(
-        chunks,
-        length=size,
-        label=f"Downloading {filename}",
-        show_percent=True,
-        show_pos=True,
-        show_eta=True,
-        width=50,
-        color="g"
-    ) as bar, open(filepath, "wb") as f:
-        for chunk in bar:
+    with open(filepath, "wb") as f:
+        pbar = tqdm(total=size, unit="B", unit_scale=True, desc=filename)
+        for chunk in res.iter_content(chunk_size=CHUNK_SIZE):
             f.write(chunk)
-            f.flush()
-            bar.update(len(chunk))
+            pbar.update(len(chunk))
 
 
-def main():
-    api_url = "https://www.mediafire.com/api/1.4/folder/get_content.php"
-    folder_api_url = "https://www.mediafire.com/api/1.4/folder/get_info.php?r=osqf"
+def download_folder(folder_url: str):
+    """Downloads the contents of the folder."""
+    api_url = "https://www.mediafire.com/api/1.5/folder/get_content.php"
+    folder_api_url = "https://www.mediafire.com/api/1.5/folder/get_info.php?r=osqf"
 
     session = requests.Session()
-    session.headers.update({
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET',
-        'Access-Control-Allow-Headers': 'Content-Type',
-        'Access-Control-Max-Age': '3600',
-        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
-    })
+    session.headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
+    }
 
-    url = sys.argv[1]
-
-    folder_key = re.search(r"folder/(.*?)/", url).group(1)
+    folder_key = re.search(r"folder/(\w+)/?", folder_url).group(1)
 
     params = {
         "r": "budo",
@@ -66,7 +60,7 @@ def main():
         "chunk": 1,
         "version": 1.5,
         "folder_key": f"{folder_key}",
-        "response_format": "json"
+        "response_format": "json",
     }
 
     response = session.get(api_url, params=params).json()
@@ -76,11 +70,17 @@ def main():
     post_data = {
         "folder_key": folder_key,
         "recursive": "yes",
-        "response_format": "json"
+        "response_format": "json",
     }
 
     folder_info = session.post(folder_api_url, post_data).json()
-    folder_name = folder_info["response"]["folder_info"]["name"]
+    folder_info = folder_info["response"]
+
+    if folder_info.get("folder_info") is None:
+        sys.stderr.write("Error: Invalid folder url")
+        return
+
+    folder_name = folder_info["folder_info"]["name"]
 
     if not os.path.exists(folder_name):
         os.mkdir(folder_name)
@@ -89,8 +89,16 @@ def main():
         download_file(content, folder_name)
 
 
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        sys.exit(f"Usage: python {__file__} <<mediafire_folder_url>>")
+def main():
+    desc = "Simple command-line script to download files from a mediafire folder."
 
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument(
+        "folder_url", nargs=1, help="Mediafire folder url to download from"
+    )
+    args = parser.parse_args()
+    download_folder(args.folder_url[0])
+
+
+if __name__ == "__main__":
     main()
